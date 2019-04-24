74b0a1ac72a6d6ef954e6a5006758c259669c498
Marko Kosunen <marko.kosunen@aalto.fi>
2019-04-23 17:15:06 -0700

    Add submodules

--- init_submodules.sh
+++ init_submodules.sh
@@ -3,24 +3,37 @@
 DIR="$( cd "$( dirname $0 )" && pwd )"
 git submodule update --init
 
-#Publish local the ones you need
-#cd $DIR/clkdiv_n_2_4_8
-#sbt publishLocal
+#!/bin/sh
+#Init submodules in this dir, if any
+DIR="$( cd "$( dirname $0 )" && pwd )"
+git submodule update --init
 
-#Selectively, init submodules of a larger projects
-#cd $DIR/rocket-chip
-#git submodule update --init chisel3
-#git submodule update --init firrtl
-#git submodule update --init hardfloat
+##Publish local the ones you need
+cd $DIR/rocket-chip
+git submodule update --init firrtl
+git submodule update --init chisel3
+git submodule update --init hardfloat
 
-#Recursively update submodeles
-#cd $DIR/eagle_serdes
-#git submodule update --init --recursive serdes_top
-#sbt publishLocal
-#Assemble executables
-###sbt publishing
-#cd $DIR/rocket-chip/firrtl
-#sbt publishLocal
-#sbt assembly
+cd $DIR/rocket-chip/firrtl
+sbt publishLocal
+cd $DIR/rocket-chip/chisel3
+sbt publishLocal
+
+cd $DIR/rocket-chip
+sbt publishLocal
+
+SUBMODULES="\
+    f2_interpolator \
+    clkmux \
+    prog_delay \
+    " 
+for module in $SUBMODULES; do
+    cd ${DIR}/${module}
+    if [ -f "./init_submodules.sh" ]; then
+        ./init_submodules.sh
+    fi
+    sbt publishLocal
+done
 
 exit 0
+
