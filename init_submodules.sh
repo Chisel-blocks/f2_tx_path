59d9c3d030a750e08b3e6974aa1767afed4818dc
Marko Kosunen <marko.kosunen@aalto.fi>
2019-04-24 18:30:08 -0700

    Remove rocket-chip submodule

--- init_submodules.sh
+++ init_submodules.sh
@@ -3,24 +3,20 @@
 DIR="$( cd "$( dirname $0 )" && pwd )"
 git submodule update --init
 
-#!/bin/sh
-#Init submodules in this dir, if any
-DIR="$( cd "$( dirname $0 )" && pwd )"
-git submodule update --init
-
-##Publish local the ones you need
-cd $DIR/rocket-chip
-git submodule update --init firrtl
-git submodule update --init chisel3
-git submodule update --init hardfloat
-
-cd $DIR/rocket-chip/firrtl
-sbt publishLocal
-cd $DIR/rocket-chip/chisel3
-sbt publishLocal
-
-cd $DIR/rocket-chip
-sbt publishLocal
+# Rocket chip is global
+###Publish local the ones you need
+#cd $DIR/rocket-chip
+#git submodule update --init firrtl
+#git submodule update --init chisel3
+#git submodule update --init hardfloat
+#
+#cd $DIR/rocket-chip/firrtl
+#sbt publishLocal
+#cd $DIR/rocket-chip/chisel3
+#sbt publishLocal
+#
+#cd $DIR/rocket-chip
+#sbt publishLocal
 
 SUBMODULES="\
     f2_interpolator \
